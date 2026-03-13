eff83cfb81a3ccf2f5e9b81702d74d91eaee63d6
sueco1 <joalm2005@gmail.com>
2026-03-13 14:15:05 +0000

    feat: add colours.h

---
+++ colours.h
@@ -0,0 +1,20 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+// Estilos
+#define BOLD    "\033[1m"
+#define RESET   "\033[0m"
+
+// Cores de Texto
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+
+// Cores de Fundo (opcional, mas útil para cabeçalhos)
+#define BG_BLUE "\033[44m"
+
+#endif
